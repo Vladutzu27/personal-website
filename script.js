a7a3f6ef10eacb9040f2c5123ae8cb35e77d321e
Vlad-Andrei Medintu <80452570+Vladutzu27@users.noreply.github.com>
2023-11-27 11:03:14 +0200

    Moved the project onto github

---
+++ script.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    var scrollTriggeredDiv = $(".scroll-triggered-div");
+    var someThreshold = 100;
+
+    
+    var header = $("header")
+    headerHeight = header.height(), 
+          logo = $(".logo"), 
+          logoHeight = logo.height(), 
+          scrollTo = 120; 
+
+    
+    $(window).on("scroll", function() {
+    
+    var yPos = $(this).scrollTop(), 
+        yPer = (yPos / scrollTo); 
+
+    
+    if (yPer > 1) {
+    yPer = 1;
+    }
+    
+    var logoPos = ( -1*(yPer*50)+50), 
+        logoSize = ((headerHeight*yPer)-(logoHeight*yPer)+logoHeight), 
+        headerPos = ((yPer * headerHeight) - headerHeight); 
+
+    
+    logo.css({
+    top: logoPos + "%",
+    left: logoPos + "%",
+    transform: "translate3d(-" + logoPos + "%,-" + logoPos + "%,0)",
+    height: logoSize
+    });
+    
+    $('header').css({
+    
+        top: headerPos,
+    opacity: yPer
+    });
+    var scrollTriggeredDiv = $(".scroll-triggered-div");
+
+    $(window).on("scroll", function() {
+        
+
+        
+        if (yPos > someThreshold) {
+            scrollTriggeredDiv.show(); 
+        } else {
+            scrollTriggeredDiv.hide(); 
+        }
+    });
+    });
+    
+}
