a5a32333e289ddd33d55d01032848145be421dea
Vladutzu27 <doarasaexperimente@gmail.com>
2025-04-02 10:13:29 +0300

    adaugat mentol

--- mentol.sh
+++ mentol.sh
@@ -22,10 +22,11 @@ echo '{
 
 echo "Setup complete! You can now compile and run C++ files with VS Code."
 
-
-gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark"
-gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y"
-gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Dark"
+wget https://github.com/vinceliuice/Orchis-theme/archive/refs/tags/2024-11-03.tar.gz
+tar -x -f 2024-11-03.tar.gz
+bash Orchis-theme-2024-11-03/install.sh 
+gsettings set org.cinnamon.desktop.interface gtk-theme "Orchis-Dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Orchis-Dark"
 
 echo "Acest script instalează aplicații în Linux Mint în funcție de preferințele tale."
 echo "Apasă Enter pentru a accepta opțiunea implicită (N)."
@@ -49,9 +50,5 @@ install_pkg "Spotify" "spotify-client"
 install_pkg "GIMP" "gimp"
 install_pkg "Steam" "steam"
 install_pkg "Wine (și toate componentele necesare)" "wine winetricks"
-
-echo "✅ Instalare finalizată!"
-
-xdg-open 'https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue%26pli%3D1%26nlr%3D1&ec=GAlAwAE&hl=ro&service=accountsettings&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1322495636%3A1743577427174892'
 
 sudo apt install sl cmatrix lolcat figlet neofetch
